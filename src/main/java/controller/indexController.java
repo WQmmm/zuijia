8e5e15cb4d606ca92b666de582d5e8546850a69d
WQmmm <1970053390@qq.com>
2020-06-30 17:39:26 +0800

    init

--- src/main/java/controller/indexController.java
+++ src/main/java/controller/indexController.java
@@ -80,5 +80,94 @@ public class indexController {
     public String toProDetail(){
         return "proDetail";
     }
-}
 
+    //跳转到商品结算页面
+    @RequestMapping("/toOrder")
+    public String toOrder(){
+        return "order";
+    }
+
+    //跳转到支付成功页面
+    @RequestMapping("/toOk")
+    public String toOk(){
+        return "ok";
+    }
+
+    //跳转到地址管理页面
+    @RequestMapping("/toAddress")
+    public String toAddress(){
+        return "address";
+    }
+
+    //跳转到抱枕专场页面
+    @RequestMapping("/toBzproList")
+    public String toBzproList(){
+        return "bzproList";
+    }
+
+    //跳转到装饰专场页面
+    @RequestMapping("/toDecoration")
+    public String toDecoration(){
+        return "decoration";
+    }
+
+    //跳转到花艺装饰专场
+    @RequestMapping("toFlowerDer")
+    public String toFlowerDer(){
+        return "flowerDer";
+    }
+
+    //跳转到找回密码页面
+    @RequestMapping("toForget")
+    public String toForget(){
+        return "forget";
+    }
+
+    //跳转到创意家居页面
+    @RequestMapping("toIdea")
+    public String toIdea(){
+        return "idea";
+    }
+
+    //跳转到个人信息页面
+    @RequestMapping("toMygrxx")
+    public String toMygrxx(){
+        return "mygrxx";
+    }
+
+    //跳转到我的订单页面
+    @RequestMapping("toMyorderq")
+    public String toMyorderq(){
+        return "myorderq";
+    }
+
+    //跳转到商品评价页面
+    @RequestMapping("toMyprod")
+    public String toMyprod(){
+        return "myprod";
+    }
+
+    //跳转到订单详情页面
+    @RequestMapping("toOrderxq")
+    public String toOrderxq(){
+        return "orderxq";
+    }
+
+    //跳转到壁式挂画页面
+    @RequestMapping("toPaint")
+    public String toPaint(){
+        return "paint";
+    }
+
+    //跳转到蜡艺香薰页面
+    @RequestMapping("toPerfume")
+    public String toPerfume(){
+        return "perfume";
+    }
+
+    //跳转到干花花艺页面
+    @RequestMapping("toProList")
+    public String toProList(){
+        return "proList";
+    }
+}
